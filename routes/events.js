1c2344942bbf9cd813dab79432e6f4c81f87404a
naw <nahuelpazberrondo@gmail.com>
2024-11-18 14:29:08 -0300

    Primer commit

---
+++ routes/events.js
@@ -0,0 +1,35 @@
+
+const { Router } = require('express');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { getEvento, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { check } = require('express-validator');
+const {validarCampos} = require('../middlewares/validar-campos');
+const isDate = require('../helpers/isDate');
+
+const router = Router();
+
+
+// Todas tienen que pasar por la validación del JWT
+router.use(validarJWT);
+
+// Obtener eventos
+router.get('/', getEvento);
+
+// Crear un nuevo evento
+router.post(
+    '/',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+        validarCampos
+    ],
+    crearEvento);
+
+// Actualizar evento
+router.put('/:id', actualizarEvento);
+
+// Borra un evento
+router.delete('/:id', eliminarEvento);
+
+module.exports = router;
